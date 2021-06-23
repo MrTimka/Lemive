561681ffb0ad9ab02cd34bee41e10e712d99dd5b
Timofei Shakhamatov <Timkant7@yandex.ru>
2021-06-23 15:15:30 +0500

    some more menu changes

--- js/menu.js
+++ js/menu.js
@@ -16,3 +16,25 @@ menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('active');
     body.classList.toggle('no-scroll');
 });
+
+const slideMenu = document.querySelector('.menu');
+
+// window.addEventListener('resize', move);
+
+// function move() {
+//   const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) / 3;
+
+//   slideMenu.style.transform = `matrix(1, 0, 0, 1, ${viewport_width}, 0)`;
+
+// //   if (viewport_width <= 968) {
+// //     scrollWidth = 660;
+// //     if (viewport_width <= 680) {
+// //       scrollWidth = viewport_width - 20;
+// //     }
+// //   } else {
+// //     scrollWidth = 970;
+// //   }
+// //   return(scrollWidth);
+// }
+
+// move();
