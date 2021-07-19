7c0f08e07e145dc7fa479f991c5ccd2ae16aab48
Timofei Shakhamatov <Timkant7@yandex.ru>
2021-07-19 17:47:01 +0500

    images with range slider

--- js/examplesRangeSlider.js
+++ js/examplesRangeSlider.js
@@ -2,25 +2,35 @@ const rangeSlider = document.querySelectorAll('.examples__range'),
       beforeImg = document.querySelectorAll('.examples__before-img'),
       image = document.querySelector('.examples__img'),
       hiddenImage = document.querySelectorAll('.examples__hidden'),
-      scrollRatio = image.getBoundingClientRect().width / 100,
-      imageCenter = image.clientWidth / 2;
+      thumb = document.querySelectorAll('.examples__thumb'),
+      thumbCenter = rangeSlider[0].offsetWidth / 2 - 22,
+      scrollRatio = rangeSlider[0].offsetWidth / 100;
 
-hiddenImage.forEach(item => {
-    item.style.minWidth = `${image.clientWidth}px`;
+      
+hiddenImage.forEach((item,i) => {
+    item.style.width = `${rangeSlider[0].offsetWidth}px`;
+    thumb[i].style.left = `${thumbCenter}px`;
 });
 
 rangeSlider.forEach((item, i) => {
     item.addEventListener('input', (event) => {
-        beforeImg[i].style.width = `${event.target.value * scrollRatio}px`;
+        beforeImg[i].style.width = `${event.target.value}%`;
+        thumb[i].style.left = `${event.target.value * scrollRatio - 22}px`;
+        beforeImg[i].style.transition = 'width 0s';
+        thumb[i].style.transition = 'left 0s';
     });
 
     item.addEventListener('mouseup', () => {
-        beforeImg[i].style.width = `${imageCenter}px`;
-        item.value = 50;
+        beforeImg[i].style.width = '50%';
+        thumb[i].style.left = `${thumbCenter}px`;
+        beforeImg[i].style.transition = 'width .3s';
+        thumb[i].style.transition = 'left .3s';
     });
 
     item.addEventListener('touchend', () => {
-        beforeImg[i].style.width = `${imageCenter}px`;
-        item.value = 50;
+        beforeImg[i].style.width = '50%';
+        thumb[i].style.left = `${thumbCenter}px`;
+        beforeImg[i].style.transition = 'width .3s';
+        thumb[i].style.transition = 'left .3s';
     });
 });
